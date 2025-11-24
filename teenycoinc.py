5ce583ae7c6a2c0e9cef7891e7bc33647d9ed03c
Goldside543 <goldside000@outlook.com>
2025-11-24 16:18:21 -0500

    Increase mining difficulty from 5 to 6

--- teenycoinc.py
+++ teenycoinc.py
@@ -192,7 +192,7 @@ class UTXOSet:
 # Block
 # ---------------------------
 class Block:
-    def __init__(self, index: int, prev_hash: str, transactions: List[Transaction], difficulty=5, timestamp=None, nonce=0):
+    def __init__(self, index: int, prev_hash: str, transactions: List[Transaction], difficulty=6, timestamp=None, nonce=0):
         self.index = index
         self.previous_hash = prev_hash
         self.transactions = transactions
@@ -280,7 +280,7 @@ class Mempool:
 # Blockchain (with UTXO, mempool, reorg)
 # ---------------------------
 class Blockchain:
-    def __init__(self, difficulty=5, mining_reward=1, max_supply=10000000, create_genesis=True):
+    def __init__(self, difficulty=6, mining_reward=1, max_supply=10000000, create_genesis=True):
         self.chain: List[Block] = []
         self.difficulty = difficulty
         self.mining_reward = mining_reward
@@ -331,6 +331,7 @@ class Blockchain:
         # update UTXO with the genesis output
         self.utxos.add_utxo(coinbase.txid, 0, genesis_out)
         self.total_supply = self.mining_reward
+        self.save_to_file()
 
     def get_last_block(self):
         return self.chain[-1]
@@ -395,6 +396,10 @@ class Blockchain:
             return False
 
     def mine_pending(self, miner_address):
+        if self.total_supply + self.mining_reward > self.max_supply:
+        print("Mining would exceed max supply, halting.")
+        return None
+        
         # create coinbase that collects fees + reward
         pending = self.mempool.get_all()
         total_fees = 0
